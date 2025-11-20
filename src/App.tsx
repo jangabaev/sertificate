a95405a70349dba456554f2266758b2721779dd8
GAMER <jangabaevmuxtar12@gmail.com>
2025-11-20 23:35:14 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,29 @@ import { Results } from "./pages/results";
 import { ResultId } from "./pages/results/resultId";
 import Navbar from "./components/layouts/navbar";
 function App() {
+  const [user, setUser] = useState("11");
   useEffect(() => {
-    const tg = window?.Telegram?.WebApp;
-    if (tg) {
+    const tg = window?.Telegram.WebApp;
+    if (tg.initDataUnsafe?.user) {
       tg.ready();
       tg.expand();
+      tg.disableVerticalSwipes(true);
+      tg.setHeaderColor("secondary_bg_color");
+      setUser(tg.initDataUnsafe?.user.id || null);
     }
   }, []);
   return (
     <>
       <Router>
+        <div className="relative z-40 bg-amber-300 h-10">
+          {Array(30)
+            .fill(0)
+            .map((_, i) => (
+              <div>{user}</div>
+            ))}
+        </div>
         <Navbar />
+
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/exam/:id" element={<ExamSend />} />
